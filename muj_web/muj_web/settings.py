7e527ab488bfd9a48dec6554007ec46e75ad1943
tomprogramuje <tom.severa@outlook.cz>
2023-04-25 14:42:45 +0200

    ver 0.1

--- muj_web/muj_web/settings.py
+++ muj_web/muj_web/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(cm%(gj5i^8hwa_m65y_4w)x%^%oq$wvk5ydtamibpjfh*7hwo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
